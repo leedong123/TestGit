53ed64475c4dda04054155a56b46c1c726ff6b07
leedong123 <leedong12138@163.com>
2018-08-03 10:22:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc,char** argv){
     const string re_name = "TestGit";
-    //print a message
+    //print two messages
     cout<<"hello c++"<<endl;
     cout<<"hello"<<re_name<<endl;
     return 0;
